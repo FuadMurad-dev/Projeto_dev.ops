86eb404fb900fab4a1e1345f4b8e4309d2508e62
Fuad Murad <murad.fuad24@gmail.com>
2025-09-29 20:10:14 -0300

    fixed: maven.yml for parallel test and switch script email for enviroments

--- scripts/sendemail.sh
+++ scripts/sendemail.sh
@@ -1,3 +1,3 @@
 cd Projeto
 sudo apt-get install mailutils
-echo "Sending email after pipeline completion" | mail -s "Pipeline" fuad.murad11@gmail.com
+echo "Sending email after pipeline completion" | mail -s "Pipeline" ${{ secret.EMAIL_DESTINY}}
