0e242231c1829719db86ec3d707f435aacc9b1ec
Fuad Murad <murad.fuad24@gmail.com>
2025-09-29 21:33:54 -0300

    fix: echo in sendemail.sh and switch code run in notification

--- scripts/sendemail.sh
+++ scripts/sendemail.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 cd Projeto
-sudo apt-get install mailutils
-echo "Sending email after pipeline completion" | mail -s "Pipeline" ${secret.EMAIL_DESTINY}
+sudo apt-get install -y mailutils
+
+EMAIL_DESTINY=$1
+echo "Sending email after pipeline completion" | mail -s "Pipeline" "$EMAIL_DESTINY"
